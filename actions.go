774d42b408b08fe5d79a09d3082bf4c83cb66c37
Leonardo <leonardo_cserny@netex.ro>
2019-02-24 19:05:11 +0200

    adds note

--- actions.go
+++ actions.go
@@ -20,6 +20,8 @@ func (a *UnknownAction) Execute(jsonFile string) (string, error) {
 	return "", errors.New("Unknown command given")
 }
 
+// TODO: add action to use from qBittorrent when done downloading to add to a db or something,
+//  then in Android app on startup it can maybe show you last finished downloading torrents
 func NewActionFrom(val string) Action {
 	switch strings.ToUpper(val) {
 	case SEARCH:
