1583080cf952a21e14086d29ebca427a569d57ab
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-02-25 15:56:32 +0200

    WIP: add basic search

--- search.go
+++ search.go
@@ -1,17 +1,57 @@
 package videosmover
 
 import (
-	"fmt"
+	"encoding/json"
+	. "github.com/lcserny/goutils"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 type SearchAction struct {
 }
 
+// TODO: put these in a shared go project `go-videosmover-shared`
+type RequestSearchData struct {
+	Path string `json:"path"`
+}
+
+type ResponseSearchData struct {
+	Path string `json:"path"`
+}
+
 func (a *SearchAction) Execute(jsonFile string) (string, error) {
-	bytes, err := ioutil.ReadFile(jsonFile)
+	jsonRequestBytes, err := ioutil.ReadFile(jsonFile)
+	LogError(err)
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("Arg: %s, had this content: %s", jsonFile, string(bytes)), nil
+
+	var request RequestSearchData
+	err = json.Unmarshal(jsonRequestBytes, &request)
+	LogError(err)
+	if err != nil {
+		return "", err
+	}
+
+	var resultList []ResponseSearchData
+	err = filepath.Walk(request.Path, func(path string, info os.FileInfo, err error) error {
+		LogError(err)
+		if !info.IsDir() {
+			resultList = append(resultList, ResponseSearchData{path})
+		}
+		return nil
+	})
+	LogError(err)
+	if err != nil {
+		return "", err
+	}
+
+	resultBytes, err := json.Marshal(resultList)
+	LogError(err)
+	if err != nil {
+		return "", err
+	}
+
+	return string(resultBytes), nil
 }
