35321e9e340ab203b6b7bc4906f515f2d3f9ac64
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-02-28 16:03:00 +0200

    adds note

--- src/actions/output.go
+++ src/actions/output.go
@@ -1,5 +1,10 @@
 package actions
 
 func OutputAction(jsonPayload []byte) (string, error) {
+	// TODO: based on a name and video type provided
+	//  trim name and such, make it decent and find it on disk
+	//  if nothing matched on disk, get a list of 10? API resolved output names from that decent name
+	//  if nothing returned from API, return the decent name (listOf 1)
+
 	return "", nil
 }
