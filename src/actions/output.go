a3080f5e20a6e9a683ac66966c3bb11dadfeac35
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-02-28 16:08:38 +0200

    adds note and structs

--- src/actions/output.go
+++ src/actions/output.go
@@ -1,7 +1,18 @@
 package actions
 
+type OutputRequestData struct {
+	Name       string `json:"name"`
+	Type       string `json:"type"`
+	MoviesPath string `json:"moviesPath"`
+	TvPath     string `json:"tvPath"`
+}
+
+type OutputResponseData struct {
+	Name string `json:"name"`
+}
+
 func OutputAction(jsonPayload []byte) (string, error) {
-	// TODO: based on a name and video type provided
+	// TODO: based on data provided
 	//  trim name and such, make it decent and find it on disk
 	//  if nothing matched on disk, get a list of 10? API resolved output names from that decent name
 	//  if nothing returned from API, return the decent name (listOf 1)
