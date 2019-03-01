38d72e2ea131f5c0c83ab494f7e46649bb14e2a3
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-01 10:44:58 +0200

    refactor actions

--- src/commander/actions/actions.go
+++ src/commander/actions/actions.go
@@ -5,10 +5,12 @@ import (
 	"strings"
 )
 
-const (
-	SEARCH = "SEARCH"
-	OUTPUT = "OUTPUT"
-)
+// TODO: add action to use from qBittorrent when done downloading to add to a db or something,
+//  then in Android app on startup it can maybe show you last finished downloading torrents
+var actionsMap = map[string]Action{
+	"SEARCH": SearchAction,
+	"OUTPUT": OutputAction,
+}
 
 type Action func(jsonPayload []byte) (string, error)
 
@@ -16,14 +18,9 @@ func UnknownAction(jsonPayload []byte) (string, error) {
 	return "", errors.New("Unknown action given")
 }
 
-// TODO: add action to use from qBittorrent when done downloading to add to a db or something,
-//  then in Android app on startup it can maybe show you last finished downloading torrents
 func NewActionFrom(val string) Action {
-	switch strings.ToUpper(val) {
-	case SEARCH:
-		return SearchAction
-	case OUTPUT:
-		return OutputAction
+	if action, ok := actionsMap[strings.ToUpper(val)]; ok {
+		return action
 	}
 	return UnknownAction
 }
