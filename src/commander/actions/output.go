3947e295b878a5db8db53a52a6f03eb7634dbf8e
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-01 15:21:52 +0200

    WIP: output -> findOnDisk done

--- src/commander/actions/output.go
+++ src/commander/actions/output.go
@@ -6,11 +6,19 @@ import (
 	. "github.com/lcserny/go-videosmover/src/shared"
 	. "github.com/lcserny/goutils"
 	"github.com/pkg/errors"
+	"os"
+	"path/filepath"
 	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-const NAME_TRIM_REGX_FILE = "name_trim_regx"
+const (
+	MAX_OUTPUT_WALK_DEPTH = 2
+	NAME_TRIM_REGX_FILE   = "name_trim_regx"
+	SIM_PERCENT_KEY       = "similarity.percent"
+)
 
 type normalizeFunc func(name string) (string, int)
 type searchTMDBFunc func(normalizedName string, year int) ([]string, bool)
@@ -20,18 +28,30 @@ type OutputConfig struct {
 	SearchTMDB searchTMDBFunc
 }
 
+type diskResult struct {
+	name        string
+	coefficient int
+}
+
 var (
-	configsMap = map[string]*OutputConfig{
+	specialCharsRegex = regexp.MustCompile(`([\[._\]])`)
+	yearPatternRegex  = regexp.MustCompile(`\s\d{4}$`)
+	configsMap        = map[string]*OutputConfig{
 		"movie": {movieNormalize, movieTMDBSearch},
 		"tv":    {tvSeriesNormalize, tvSeriesTMDBSearch},
 	}
 	nameTrimPartsRegxs []*regexp.Regexp
+	acceptedSimPercent int
 )
 
 func init() {
 	nameTrimPartsContent, err := configFolder.FindString(NAME_TRIM_REGX_FILE)
 	LogError(err)
 	nameTrimPartsRegxs = getRegexList(GetLinesFromString(nameTrimPartsContent))
+
+	if appProperties.HasProperty(SIM_PERCENT_KEY) {
+		acceptedSimPercent = appProperties.GetPropertyAsInt(SIM_PERCENT_KEY)
+	}
 }
 
 func OutputAction(jsonPayload []byte) (string, error) {
@@ -86,15 +106,74 @@ func getConfig(itemType string) (*OutputConfig, error) {
 }
 
 func movieNormalize(name string) (string, int) {
-	return "", 0
+	normalized, _ := tvSeriesNormalize(name)
+
+	// resolve year
+	yearLoc := yearPatternRegex.FindStringIndex(normalized)
+	if yearLoc != nil {
+		year, err := strconv.ParseInt(normalized[yearLoc[0]+1:], 0, 32)
+		LogError(err)
+		return normalized[0:yearLoc[0]], int(year)
+	}
+
+	return normalized, 0
 }
 
 func tvSeriesNormalize(name string) (string, int) {
-	return "", 0
+	// trim
+	for _, pat := range nameTrimPartsRegxs {
+		if loc := pat.FindStringIndex(name); loc != nil {
+			name = name[0:loc[0]]
+		}
+	}
+
+	// strip special chars
+	name = specialCharsRegex.ReplaceAllString(name, " ")
+	name = strings.Trim(name, " ")
+
+	// title case
+	name = strings.Title(name)
+
+	return name, 0
 }
 
-func findOnDisk(normalizedWithYear, diskPath string) ([]string, bool) {
-	return nil, false
+func findOnDisk(normalizedWithYear, diskPath string) (results []string, found bool) {
+	var tmpList []diskResult
+	err := filepath.Walk(diskPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			LogError(err)
+			return nil
+		}
+
+		if info.IsDir() && diskPath != path && walkDepthIsAcceptable(diskPath, path, MAX_OUTPUT_WALK_DEPTH) {
+			distance := LevenshteinDistance(info.Name(), normalizedWithYear)
+			bigger := MaxInt(len(normalizedWithYear), len(info.Name()))
+			simPercent := int(float32(bigger-distance) / float32(bigger) * 100)
+			if simPercent > acceptedSimPercent {
+				tmpList = append(tmpList, diskResult{info.Name(), simPercent})
+			}
+		}
+
+		return nil
+	})
+	LogError(err)
+	if err != nil {
+		return results, false
+	}
+
+	if len(tmpList) < 1 {
+		return results, false
+	}
+
+	// sort by highest coefficient
+	sort.Slice(tmpList, func(i, j int) bool {
+		return tmpList[i].coefficient > tmpList[j].coefficient
+	})
+	for _, ele := range tmpList {
+		results = append(results, ele.name)
+	}
+
+	return results, true
 }
 
 func movieTMDBSearch(normalizedName string, year int) ([]string, bool) {
--- src/commander/actions/shared.go
+++ src/commander/actions/shared.go
@@ -3,10 +3,22 @@ package actions
 import (
 	"encoding/json"
 	. "github.com/lcserny/goutils"
+	"os"
 )
 
 func getJSONEncodedString(data interface{}) string {
 	resultBytes, err := json.Marshal(data)
 	LogError(err)
 	return string(resultBytes)
+}
+
+func walkDepthIsAcceptable(rootPath string, path string, maxWalkDepth int) bool {
+	trimmed := path[len(rootPath):]
+	separatorCount := 0
+	for _, char := range trimmed {
+		if char == os.PathSeparator {
+			separatorCount++
+		}
+	}
+	return separatorCount < maxWalkDepth
 }
