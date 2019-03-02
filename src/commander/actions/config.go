73a6d8e33ac9a184ee809022c3d82917d67b256c
Leonardo Cserny <leo.cserny@gmail.com>
2019-03-02 20:09:43 +0200

    refactor

--- src/commander/actions/config.go
+++ src/commander/actions/config.go
@@ -3,8 +3,10 @@ package actions
 import (
 	"github.com/gobuffalo/packr"
 	. "github.com/lcserny/goutils"
+	"github.com/pkg/errors"
 	"log"
 	"os"
+	"strings"
 )
 
 const (
@@ -16,7 +18,15 @@ const (
 var (
 	appProperties *ConfigProperties
 	configFolder  packr.Box
+	// TODO: add action to use from qBittorrent when done downloading to add to a db or something,
+	//  then in Android app on startup it can maybe show you last finished downloading torrents
+	actionsMap = map[string]Action{
+		"SEARCH": SearchAction,
+		"OUTPUT": OutputAction,
+	}
 )
+
+type Action func(jsonPayload []byte) (string, error)
 
 func init() {
 	initLogger()
@@ -32,4 +42,15 @@ func initLogger() {
 	openFile, err := os.OpenFile(LOG_FILE, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	LogFatal(err)
 	log.SetOutput(openFile)
+}
+
+func UnknownAction(jsonPayload []byte) (string, error) {
+	return "", errors.New("Unknown action given")
+}
+
+func NewActionFrom(val string) Action {
+	if action, ok := actionsMap[strings.ToUpper(val)]; ok {
+		return action
+	}
+	return UnknownAction
 }
