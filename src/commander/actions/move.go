1720ca9c635f92aba5d67da5f9eb5f38694725f9
Leonardo Cserny <leo.cserny@gmail.com>
2019-03-03 13:46:13 +0200

    refactored move

--- src/commander/actions/move.go
+++ src/commander/actions/move.go
@@ -23,13 +23,91 @@ const (
 
 var restrictedRemovePaths []string
 
+type moveExecutor struct {
+	resultList   *[]MoveResponseData
+	request      *MoveRequestData
+	folder, dest string
+}
+
+func (me *moveExecutor) canProceedWithError(err error, reason string) bool {
+	if err != nil {
+		LogError(err)
+		*me.resultList = append(*me.resultList, MoveResponseData{me.folder, []string{reason}})
+		return false
+	}
+	return true
+}
+
+func (me *moveExecutor) prepareMove() bool {
+	if _, err := os.Stat(me.dest); os.IsNotExist(err) {
+		err := os.MkdirAll(me.dest, os.ModePerm)
+		if err != nil {
+			*me.resultList = append(*me.resultList, MoveResponseData{me.folder,
+				[]string{fmt.Sprintf(COULDNT_CREATE_FOLDER_REASON, me.dest)}})
+			return false
+		}
+	} else {
+		if me.request.Type == MOVIE {
+			*me.resultList = append(*me.resultList, MoveResponseData{me.folder,
+				[]string{fmt.Sprintf(MOVIE_EXISTS_REASON, me.dest, me.request.DiskPath)}})
+			return false
+		}
+	}
+	return true
+}
+
+func (me *moveExecutor) moveSubs() bool {
+	unmovedSubs := false
+	var unmovedSubsReasons []string
+
+	for _, sub := range me.request.Subs {
+		trimmedSub := strings.Replace(sub, me.folder, "", 1)
+		subDest := filepath.Join(me.dest, trimmedSub)
+		// move sub
+		err := os.Rename(sub, subDest)
+		if err != nil {
+			unmovedSubs = true
+			LogError(err)
+			unmovedSubsReasons = append(unmovedSubsReasons, fmt.Sprintf(MOVING_PROBLEM_REASON, sub))
+			continue
+		}
+	}
+
+	if unmovedSubs {
+		*me.resultList = append(*me.resultList, MoveResponseData{me.folder, unmovedSubsReasons})
+		return false
+	}
+
+	return true
+}
+
+func (me *moveExecutor) cleanIfPossible() {
+	restricted := false
+	for _, restrictedFolder := range restrictedRemovePaths {
+		if strings.Index(me.folder, restrictedFolder) != -1 {
+			restricted = true
+			*me.resultList = append(*me.resultList, MoveResponseData{me.folder,
+				[]string{fmt.Sprintf(RESTRICTED_PATH_REASON, me.folder)}})
+			break
+		}
+	}
+	if !restricted {
+		err := os.RemoveAll(me.folder)
+		if err != nil {
+			LogError(err)
+			*me.resultList = append(*me.resultList, MoveResponseData{me.folder,
+				[]string{fmt.Sprintf(COULDNT_REMOVE_FOLDER_REASON, me.folder)}})
+		}
+	}
+}
+
 func init() {
 	restrictedRemovePathsContent, err := configFolder.FindString(RESTRICTED_REMOVE_PATHS_FILE_KEY)
 	LogError(err)
 	restrictedRemovePaths = GetLinesFromString(restrictedRemovePathsContent)
 }
 
-// TODO: test and refactor
+// TODO: test
 func MoveAction(jsonPayload []byte) (string, error) {
 	var request []MoveRequestData
 	err := json.Unmarshal(jsonPayload, &request)
@@ -40,78 +118,37 @@ func MoveAction(jsonPayload []byte) (string, error) {
 
 	var resultList []MoveResponseData
 	for _, req := range request {
-		videoDir := filepath.Dir(req.Video)
-		dest := filepath.Join(req.DiskPath, req.OutName)
-
-		if _, err := os.Stat(dest); os.IsNotExist(err) {
-			err := os.MkdirAll(dest, os.ModePerm)
-			if err != nil {
-				resultList = append(resultList, MoveResponseData{videoDir,
-					[]string{fmt.Sprintf(COULDNT_CREATE_FOLDER_REASON, dest)}})
-				continue
-			}
-		} else {
-			if req.Type == MOVIE {
-				resultList = append(resultList, MoveResponseData{videoDir,
-					[]string{fmt.Sprintf(MOVIE_EXISTS_REASON, dest, req.DiskPath)}})
-				continue
-			}
+		moveExecutor := moveExecutor{
+			&resultList,
+			&req,
+			filepath.Dir(req.Video),
+			filepath.Join(req.DiskPath, req.OutName),
 		}
 
+		// prepare
+		if proceed := moveExecutor.prepareMove(); !proceed {
+			continue
+		}
+
+		// get info
 		info, err := os.Stat(req.Video)
-		if err != nil {
-			LogError(err)
-			resultList = append(resultList, MoveResponseData{videoDir,
-				[]string{fmt.Sprintf(INPUT_VIDEO_PROBLEM_REASON, req.Video)}})
+		if proceed := moveExecutor.canProceedWithError(err, fmt.Sprintf(INPUT_VIDEO_PROBLEM_REASON, req.Video)); !proceed {
 			continue
 		}
 
 		// move video
-		err = os.Rename(req.Video, filepath.Join(dest, info.Name()))
-		if err != nil {
-			LogError(err)
-			resultList = append(resultList, MoveResponseData{videoDir,
-				[]string{fmt.Sprintf(MOVING_PROBLEM_REASON, req.Video)}})
+		err = os.Rename(req.Video, filepath.Join(moveExecutor.dest, info.Name()))
+		if proceed := moveExecutor.canProceedWithError(err, fmt.Sprintf(MOVING_PROBLEM_REASON, req.Video)); !proceed {
 			continue
 		}
 
-		// move subs, forEach
-		unmovedSubs := false
-		var unmovedSubsReasons []string
-		for _, sub := range req.Subs {
-			trimmedSub := strings.Replace(sub, videoDir, "", 1)
-			subDest := filepath.Join(dest, trimmedSub)
-			err = os.Rename(sub, subDest)
-			if err != nil {
-				unmovedSubs = true
-				LogError(err)
-				unmovedSubsReasons = append(unmovedSubsReasons, fmt.Sprintf(MOVING_PROBLEM_REASON, sub))
-				continue
-			}
-		}
-		if unmovedSubs {
-			resultList = append(resultList, MoveResponseData{videoDir, unmovedSubsReasons})
+		// move subs
+		if proceed := moveExecutor.moveSubs(); !proceed {
 			continue
 		}
 
-		// clean folder if possible (not restricted path or such)
-		restricted := false
-		for _, restrictedFolder := range restrictedRemovePaths {
-			if strings.Index(videoDir, restrictedFolder) != -1 {
-				restricted = true
-				resultList = append(resultList, MoveResponseData{videoDir,
-					[]string{fmt.Sprintf(RESTRICTED_PATH_REASON, videoDir)}})
-				break
-			}
-		}
-		if !restricted {
-			err := os.RemoveAll(videoDir)
-			if err != nil {
-				LogError(err)
-				resultList = append(resultList, MoveResponseData{videoDir,
-					[]string{fmt.Sprintf(COULDNT_REMOVE_FOLDER_REASON, videoDir)}})
-			}
-		}
+		// clean
+		moveExecutor.cleanIfPossible()
 	}
 
 	return getJSONEncodedString(resultList), nil
