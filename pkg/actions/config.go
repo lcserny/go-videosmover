7dc767d10312b399970a9cd3402757edc3759574
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-14 14:55:03 +0200

    adds note

--- pkg/actions/config.go
+++ pkg/actions/config.go
@@ -14,6 +14,12 @@ import (
 // TODO: add action to use from qBittorrent when done downloading to add to a db or something,
 //  then in Android app on startup it can maybe show you last finished downloading torrents
 //  For this to work, enable qBittorrent WebUI (bypass localhost login and set port) to issue delete requests from torrent list on download complete
+
+// Example delete torrent
+/*POST http://localhost:8078/command/delete
+Content-Type: application/x-www-form-urlencoded
+hashes=e60bc7149e5c0e9b32f60cefb7c63bad303ceca6*/
+
 var actionsMap = map[string]Action{
 	"SEARCH": SearchAction,
 	"OUTPUT": OutputAction,
