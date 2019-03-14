2259103aedbe94d0e9d964c57830f04131c10cff
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-14 11:32:27 +0200

    adds more search tests

--- pkg/actions/search_test.go
+++ pkg/actions/search_test.go
@@ -14,12 +14,14 @@ func TestSearchAction(t *testing.T) {
 	video2 := addVideo(t, path, "video2.mp4")
 	_ = addSubtitles(t, video2, []string{"subtitle.srt"})
 	video3 := addVideo(t, path, filepath.Join("A-Video1", "video1.mp4"))
+	hiddenVideo4 := addVideo(t, path, "NonVideo.txt")
 
 	testData := []testActionData{
 		{
 			request: SearchRequestData{path},
 			response: []SearchResponseData{
 				{video3, make([]string, 0)},
+				{hiddenVideo4, make([]string, 0)},
 				{video1, video1Subs},
 				{video2, make([]string, 0)},
 			},
