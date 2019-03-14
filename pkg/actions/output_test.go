725e87bd7d4acb754a78da2035683235d6c2ffa8
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-14 09:35:38 +0200

    adds output action test

--- pkg/actions/output_test.go
+++ pkg/actions/output_test.go
@@ -8,8 +8,8 @@ import (
 func TestOutputAction(t *testing.T) {
 	testData := []testActionData{
 		{
-			request:  OutputRequestData{Name: "", Type: "", SkipOnlineSearch: true},
-			response: OutputResponseData{[]string{"", ""}, ORIGIN_TMDB},
+			request:  OutputRequestData{Name: "The Lord of the Rings: The Fellowship of <>the Ring (2001)", Type: "movie", SkipOnlineSearch: true},
+			response: OutputResponseData{[]string{"The Lord Of The Rings The Fellowship Of The Ring (2001)"}, ORIGIN_NAME},
 		},
 	}
 
