8307f482af954dde70801e0a5eb2f56120da7cd1
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-18 13:35:31 +0200

    centers webview window

--- cmd/webview.go
+++ cmd/webview.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/sciter-sdk/go-sciter"
 	"github.com/sciter-sdk/go-sciter/window"
 	"log"
+	"os"
 )
 
+var screenWidth = flag.Int("screenWidth", 0, "screen width")
+var screenHeight = flag.Int("screenHeight", 0, "screen height")
+
 func main() {
-	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, nil)
+	args := os.Args[1:]
+	if len(args) != 2 {
+		_, _ = fmt.Fprintln(os.Stderr, "ERROR: Please provide `screenWidth` and `screenHeight` flags")
+		return
+	}
+
+	flag.Parse()
+
+	windowWidth, windowHeight := 400, 400
+	top, left := (*screenHeight/2)-(windowHeight/2), (*screenWidth/2)-(windowWidth/2)
+
+	rect := sciter.NewRect(top, left, windowWidth, windowHeight)
+	w, err := window.New(sciter.DefaultWindowCreateFlag, rect)
 	if err != nil {
 		log.Fatal(err)
 	}
