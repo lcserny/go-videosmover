9a4179d8695fdc8cc3802777d9fd783a1ea2e850
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-03-18 12:43:40 +0200

    switches to go-sciter

--- cmd/webview.go
+++ cmd/webview.go
@@ -1,114 +1,19 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/zserge/webview"
-	"io/ioutil"
+	"github.com/sciter-sdk/go-sciter"
+	"github.com/sciter-sdk/go-sciter/window"
 	"log"
-	"net"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 )
 
-const (
-	windowWidth  = 480
-	windowHeight = 320
-)
-
-var htmlPathFlag = flag.String("htmlPath", "", "path to html files")
-
-func startServer() string {
-	ln, err := net.Listen("tcp", "127.0.0.1:0")
+func main() {
+	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func() {
-		defer ln.Close()
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			bytes, _ := ioutil.ReadFile(filepath.Join(*htmlPathFlag, "index.html"))
-			w.Write(bytes)
-		})
-		log.Fatal(http.Serve(ln, nil))
-	}()
-	return "http://" + ln.Addr().String()
-}
-
-func handleRPC(w webview.WebView, data string) {
-	switch {
-	case data == "close":
-		w.Terminate()
-	case data == "fullscreen":
-		w.SetFullscreen(true)
-	case data == "unfullscreen":
-		w.SetFullscreen(false)
-	case data == "open":
-		log.Println("open", w.Dialog(webview.DialogTypeOpen, 0, "Open file", ""))
-	case data == "opendir":
-		log.Println("open", w.Dialog(webview.DialogTypeOpen, webview.DialogFlagDirectory, "Open directory", ""))
-	case data == "save":
-		log.Println("save", w.Dialog(webview.DialogTypeSave, 0, "Save file", ""))
-	case data == "message":
-		w.Dialog(webview.DialogTypeAlert, 0, "Hello", "Hello, world!")
-	case data == "info":
-		w.Dialog(webview.DialogTypeAlert, webview.DialogFlagInfo, "Hello", "Hello, info!")
-	case data == "warning":
-		w.Dialog(webview.DialogTypeAlert, webview.DialogFlagWarning, "Hello", "Hello, warning!")
-	case data == "error":
-		w.Dialog(webview.DialogTypeAlert, webview.DialogFlagError, "Hello", "Hello, error!")
-	case strings.HasPrefix(data, "changeTitle:"):
-		w.SetTitle(strings.TrimPrefix(data, "changeTitle:"))
-	case strings.HasPrefix(data, "changeColor:"):
-		hex := strings.TrimPrefix(strings.TrimPrefix(data, "changeColor:"), "#")
-		num := len(hex) / 2
-		if !(num == 3 || num == 4) {
-			log.Println("Color must be RRGGBB or RRGGBBAA")
-			return
-		}
-		i, err := strconv.ParseUint(hex, 16, 64)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if num == 3 {
-			r := uint8((i >> 16) & 0xFF)
-			g := uint8((i >> 8) & 0xFF)
-			b := uint8(i & 0xFF)
-			w.SetColor(r, g, b, 255)
-			return
-		}
-		if num == 4 {
-			r := uint8((i >> 24) & 0xFF)
-			g := uint8((i >> 16) & 0xFF)
-			b := uint8((i >> 8) & 0xFF)
-			a := uint8(i & 0xFF)
-			w.SetColor(r, g, b, a)
-			return
-		}
-	}
-}
-
-func main() {
-	args := os.Args[1:]
-	if len(args) != 1 {
-		_, _ = fmt.Fprintln(os.Stderr, "ERROR: Please provide `htmlPath` flag")
-		return
-	}
-
-	flag.Parse()
-	url := startServer()
-	w := webview.New(webview.Settings{
-		Width:                  windowWidth,
-		Height:                 windowHeight,
-		Title:                  "Simple window demo",
-		Resizable:              true,
-		URL:                    url,
-		ExternalInvokeCallback: handleRPC,
-	})
-	w.SetColor(255, 255, 255, 255)
-	defer w.Exit()
+	// log.Printf("handle: %v", w.Handle)
+	w.LoadFile("static/html/simple.html")
+	w.SetTitle("Example")
+	w.Show()
 	w.Run()
 }
