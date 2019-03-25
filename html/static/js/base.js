a7180d4e2aec45b666dbebd694302752311b9755
Leonardo Cserny <leo.cserny@gmail.com>
2019-03-25 20:18:01 +0200

    adjusts gohtml

--- html/static/js/base.js
+++ html/static/js/base.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $("input.js-videoTypeInput").change(function () {
         // TODO: ajax post to /ajax/output sending needed data
-        var $data = $(this).parents("tr");
-        var type = $(this).val();
-        console.log($data.data());
-        console.log(type);
+        var index = $(this).data("index");
+        var $data = $("#js-videoRow" + index).data();
+        $data["type"] = $(this).val();
+        console.log($data);
     });
 });
