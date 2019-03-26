82fad59436c5ba792391a3da4c3510616cc10256
Leonardo Cserny <leo.cserny@gmail.com>
2019-03-26 13:44:08 +0200

    adds skipcache and skiponlinesearch to html

--- html/static/js/base.js
+++ html/static/js/base.js
@@ -1,9 +1,20 @@
+function addToRowData(that, key, val) {
+    var index = $(that).data("index");
+    var data = $("#js-videoRow" + index).data();
+    data[key] = val;
+}
+
 $(document).ready(function () {
     $("input.js-videoTypeInput").change(function () {
+        addToRowData(this, "type", $(this).val());
         // TODO: ajax post to /ajax/output sending needed data
-        var index = $(this).data("index");
-        var $data = $("#js-videoRow" + index).data();
-        $data["type"] = $(this).val();
-        console.log($data);
+    });
+
+    $("input.js-videoSkipCacheInput").change(function () {
+        addToRowData(this, "skipcache", $(this).is(":checked"));
+    });
+
+    $("input.js-videoSkipOnlineSearchInput").change(function () {
+        addToRowData(this, "skiponlinesearch", $(this).is(":checked"));
     });
 });
