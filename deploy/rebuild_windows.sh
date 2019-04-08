fd8fee720043f8954c9aa1d51be9c9f65ab1978c
Leonardo <leonardo_cserny@netex.ro>
2019-04-08 20:38:40 +0300

    adds rebuild deploy script

---
+++ deploy/rebuild_windows.sh
@@ -0,0 +1,7 @@
+#!bash
+
+cd ..
+go build -ldflags -H=windowsgui -o=../bin/commander.exe cmd/commander.go
+go build -ldflags -H=windowsgui -o=../bin/proxy_server.exe cmd/proxy_server.go
+go build -ldflags -H=windowsgui -o=../bin/webview.exe cmd/webview.go
+go build -ldflags -H=windowsgui -o=../bin/remove_from_qtorrent.exe cmd/remove_from_qtorrent.go
