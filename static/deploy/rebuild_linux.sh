cd5706d7228038b926d3cf403cf6063eac26b97c
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-05-08 13:43:31 +0300

    includes tests in build

--- static/deploy/rebuild_linux.sh
+++ static/deploy/rebuild_linux.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd ../..
+
+go test -v videosmover/pkg/move
+go test -v videosmover/pkg/delete
+go test -v videosmover/pkg/search
+go test -v videosmover/pkg/output
+
 go build -o=../bin/commander cmd/commander.go
 go build -o=../bin/proxy_server cmd/proxy_server.go
 go build -o=../bin/webview cmd/webview.go
--- static/deploy/rebuild_windows.sh
+++ static/deploy/rebuild_windows.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd ../..
+
+go test -v videosmover/pkg/move
+go test -v videosmover/pkg/delete
+go test -v videosmover/pkg/search
+go test -v videosmover/pkg/output
+
 go build -ldflags -H=windowsgui -o=../bin/commander.exe cmd/commander.go
 go build -ldflags -H=windowsgui -o=../bin/proxy_server.exe cmd/proxy_server.go
 go build -ldflags -H=windowsgui -o=../bin/webview.exe cmd/webview.go
