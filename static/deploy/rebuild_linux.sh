2367aa6050a0eb6cd25126baa34239e63ba9157b
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-05-10 12:32:24 +0300

    updates deploy scripts

--- static/deploy/rebuild_linux.sh
+++ static/deploy/rebuild_linux.sh
@@ -2,11 +2,11 @@
 
 cd ../..
 
-go test -v videosmover/pkg/action
-go test -v videosmover/pkg/move
-go test -v videosmover/pkg/delete
-go test -v videosmover/pkg/search
-go test -v videosmover/pkg/output
+go test videosmover/pkg/action
+go test videosmover/pkg/move
+go test videosmover/pkg/delete
+go test videosmover/pkg/search
+go test videosmover/pkg/output
 
 go install cmd/commander.go
 go install cmd/proxy_server.go
