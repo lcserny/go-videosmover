b30163f5d37e1933aded80256a8b44bfa14f7263
Leonardo Cserny <leo.cserny@gmail.com>
2019-05-16 19:09:57 +0300

    WIP search JS

--- static/html/js/search.js
+++ static/html/js/search.js
@@ -1,14 +1,62 @@
-class SearchProcessor {
-    // try without jquery?
-    constructor(
-        // init webstorage & loop over all rows/data if there and add to storage
-    )
+class VideoData {
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+    }
 
-    registerHandlers(doc) {
+    getName() {
+        return this.name;
+    }
 
+    getValue() {
+        return this.value;
+    }
+}
+
+class VideoDataRepository {
+    constructor() {
+        // init in memory list storage
+        this.list = [];
+    }
+
+    add(videoData) {
+        if (videoData instanceof VideoData) {
+            this.list.push(videoData);
+        }
+    }
+
+    get(index) {
+        let videoData = this.list.get(index);
+        if (videoData instanceof VideoData) {
+            return videoData;
+        }
+        return null;
+    }
+}
+
+class VideoDataService {
+    constructor(repo) {
+        this.repo = repo;
+    }
+}
+
+class SearchHandler {
+    constructor(service) {
+        this.service = service;
+        // loop over all rows/data if there and use service to convert rows to videoData and add to storage
+    }
+
+    register() {
+        // register event handlers on document
+        const videoTypeRadios = document.querySelectorAll('.js-videoRow .js-videoTypeInput');
+        videoTypeRadios.forEach((radio) => {
+            radio.addEventListener('change', (event) => {
+                alert('Thanks for clicking! ' + radio.value);
+            });
+        });
     }
 }
 
 $(document).ready(function () {
-    new SearchProcessor().registerHandlers(document);
+    new SearchHandler(new VideoDataService(new VideoDataRepository())).register();
 });
