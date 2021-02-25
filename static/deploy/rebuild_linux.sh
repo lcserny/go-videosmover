6d44cba5819cb2862881bbe11fe79e103e7517d6
Leonardo Cserny <leonardo.cserny@endava.com>
2021-02-25 12:15:12 +0200

    improved deploy scripts and skipped external TMDB tests

--- static/deploy/rebuild_linux.sh
+++ static/deploy/rebuild_linux.sh
@@ -2,23 +2,29 @@
 
 cd ../..
 
+echo "Running tests"
+
 go test videosmover/pkg/action
 go test videosmover/pkg/move
 go test videosmover/pkg/delete
 go test videosmover/pkg/search
 go test videosmover/pkg/output
 
-cd cmd/commander && go install
+echo "Building apps in output folder"
+
+cd cmd/commander && go build -o ../../static/deploy/output/commander
 cd ../..
 
-cd cmd/proxy_server && go install
+cd cmd/proxy_server && go build -o ../../static/deploy/output/proxy_server
 cd ../..
 
-cd cmd/webview && go install
+cd cmd/webview && go build -o ../../static/deploy/output/webview
 cd ../..
 
-cd cmd/remove_from_qtorrent && go install
+cd cmd/remove_from_qtorrent && go build -o ../../static/deploy/output/remove_from_qtorrent
 cd ../..
 
-cd cmd/cache_server && go install
+cd cmd/cache_server && go build -o ../../static/deploy/output/cache_server
 cd ../..
+
+echo "Done!"
