8671a0a19cc457615068f0fd552dc1ec9124a1d9
Leonardo Cserny <leo.cserny@gmail.com>
2021-06-20 11:00:37 +0300

    minor refactor

--- tstest/service.test.ts
+++ tstest/service.test.ts
@@ -1,10 +1,12 @@
 import {assert, expect} from "chai";
 import {describe, it} from "mocha";
 import {BasicVideoDataService} from "../tssrc/VideoDataService";
+import {InMemoryVideoDataRepository} from "../tssrc/VideoDataRepository";
 
 describe("service init", function () {
     it("new service is not null", function () {
-        let service = new BasicVideoDataService(null);
+        let repo = new InMemoryVideoDataRepository();
+        let service = new BasicVideoDataService(repo);
         assert.notEqual(service, null);
     });
 });
