4b1105afcc4d0f1f53e45205a0785fa898d34389
Leonardo Cserny <leo.cserny@gmail.com>
2022-09-25 22:30:11 +0300

    update project

--- pkg/shutdown/shutdown.go
+++ pkg/shutdown/shutdown.go
@@ -3,11 +3,19 @@ package shutdown
 import (
 	"bytes"
 	"os/exec"
+	"runtime"
 )
 
 func Shutdown(seconds string) {
 	var cmdErr bytes.Buffer
-	cmd := exec.Command("cmd", "/C", "shutdown", "-s", "-t", seconds)
+	os := runtime.GOOS
+	var cmd *exec.Cmd
+	switch os {
+	case "windows":
+		cmd = exec.Command("cmd", "/C", "shutdown", "-s", "-t", seconds)
+	case "linux":
+		cmd = exec.Command("poweroff")
+	}
 	cmd.Stderr = &cmdErr
 	if err := cmd.Run(); err != nil {
 		cmdErr.WriteString(err.Error())
